35d67a52b6b7b4cb13f26664d4cd3ca3f94735f8
Davyd Kohut <expexes@gmail.com>
2019-03-19 21:29:20 +0200

    gix

--- telegroo/src/main/groovy/tech/teslex/telegroo/simple/update/SimpleCommandUpdateHandler.groovy
+++ telegroo/src/main/groovy/tech/teslex/telegroo/simple/update/SimpleCommandUpdateHandler.groovy
@@ -1,9 +1,9 @@
 package tech.teslex.telegroo.simple.update
 
 import groovy.transform.CompileStatic
-import tech.teslex.telegroo.api.update.MessageUpdateHandler
+import tech.teslex.telegroo.api.update.CommandUpdateHandler
 import tech.teslex.telegroo.simple.context.MethodsContext
 
 @CompileStatic
-interface SimpleCommandUpdateHandler extends MessageUpdateHandler<MethodsContext> {
+interface SimpleCommandUpdateHandler extends CommandUpdateHandler<MethodsContext> {
 }
